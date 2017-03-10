962993e1181bcfeef46adad74fd9ba32bea02afa
roc-niek <niekpoorthuis@hotmail.com>
2017-03-10 13:20:05 +0100

    Add files via upload

---
+++ Opdracht3/Default.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+    </div>
+        <asp:Calendar ID="KalVerjaardag" runat="server" OnSelectionChanged="KalVerjaardag_SelectionChanged" SelectedDate="1995-01-01"></asp:Calendar>
+        <p>
+            <asp:Label ID="Label1" runat="server" Text=" "></asp:Label>
+        </p>
+        <p>
+            <asp:Label ID="Label2" runat="server" Text=" "></asp:Label>
+        </p>
+        <p>
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Laat zien" />
+        </p>
+    </form>
+</body>
+</html>
---
+++ Opdracht3/Default.aspx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    /*
+     * 
+     * Label1 is de geselecteerde datum
+     * Label2 is de leeftijd
+     * 
+     * */
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        DateTime datum = KalVerjaardag.SelectedDate;
+        Label1.Text = "U heeft de onderstaande datum geselecteerd:<br><br> " + datum;
+        Label2.Text = "Uw leeftijd is:<br><br>" + getLeeftijd(datum);
+    }
+
+    protected int getLeeftijd(DateTime datum)
+    {
+        //De dag van vandaag
+        var today = DateTime.UtcNow;
+        // bereken de leeftijd
+        var leeftijd = today.Year - datum.Year;
+        if (datum > today.AddYears(-leeftijd))
+            leeftijd--;
+        return leeftijd;
+    }
+    protected void KalVerjaardag_SelectionChanged(object sender, EventArgs e)
+    {
+
+    }
+}

d9cafb943223f5cd5224d300c3660c6a14ca9b84
roc-niek <niekpoorthuis@hotmail.com>
2017-03-24 14:42:59 +0100

    Add files via upload

---
+++ Opdracht6/Default.aspx
@@ -0,0 +1,71 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            height: 393px;
+        }
+    </style>
+</head>
+<body style="height: 375px">
+    <form id="form1" runat="server">
+        <p>
+            De rekenmachine</p>
+        <p>
+            &nbsp;Hieronder staat een grafische rekenmachine. Druk op de knoppen om deze te gebruiken.<br />
+        </p>
+        <table style="width: 100%; height: 100%;">
+            <tr>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td class="auto-style1">
+                    <asp:TextBox ID="txtScherm" runat="server" style="margin-top: 0px" Width="250px" Height="50px"></asp:TextBox>
+                    <br />
+                    <asp:Button ID="btn7" runat="server" Text="7" Width="50px" Height="40px" OnClick="btn7_Click"/>
+                    <asp:Button ID="btn8" runat="server" Text="8" Width="50px" Height="40px" OnClick="btn8_Click"/>
+                    <asp:Button ID="btn9" runat="server" Text="9" Width="50px" Height="40px" OnClick="btn9_Click"/>
+                    <asp:Button ID="btnC" runat="server" Text="C" Width="50px" Height="40px" OnClick="btnC_Click"/>
+                    <asp:Button ID="btnBack" runat="server" Text="Back" Width="50px" Height="40px" OnClick="btnBack_Click"/>
+                    <br />
+                    <asp:Button ID="btn4" runat="server" Text="4" Width="50px" Height="40px" OnClick="btn4_Click"/>
+                    <asp:Button ID="btn5" runat="server" Text="5" Width="50px" Height="40px" OnClick="btn5_Click"/>
+                    <asp:Button ID="btn6" runat="server" Text="6" Width="50px" Height="40px" OnClick="btn6_Click"/>
+                    <asp:Button ID="btnPlus" runat="server" Text="+" Width="50px" Height="40px" OnClick="btnPlus_Click"/>
+                    <asp:Button ID="btnM2" runat="server" Text="M2" Width="50px" Height="40px"/>
+                    <br />
+                    <asp:Button ID="btn1" runat="server" Text="1" Width="50px" Height="40px" OnClick="btn1_Click"/>
+                    <asp:Button ID="btn2" runat="server" Text="2" Width="50px" Height="40px" OnClick="btn2_Click"/>
+                    <asp:Button ID="btn3" runat="server" Text="3" Width="50px" Height="40px" OnClick="btn3_Click"/>
+                    <asp:Button ID="btnMin" runat="server" Text="-" Width="50px" Height="40px"/>
+                    <asp:Button ID="btnO1" runat="server" Text="O1" Width="50px" Height="40px"/>
+                    <br />
+                    <asp:Button ID="btn0" runat="server" Text="0" Width="100px" Height="40px" OnClick="btn0_Click"/>
+                    <asp:Button ID="btnPunt" runat="server" Text="." Width="50px"  Height="40px" OnClick="btnPunt_Click"/>
+                    <asp:Button ID="btnIs" runat="server" Text="=" Width="50px"  Height="40px" OnClick="btnIs_Click"/>
+                    <asp:Button ID="btnO2" runat="server" Text="O2" Width="50px" Height="40px"/>
+                    <br />
+                    <br />
+                    <asp:Label ID="lbl_max" runat="server" Text="Maximaal 7 nummers." Visible="False"></asp:Label>
+                    <br />
+                    <asp:Label ID="lGetal1" runat="server" Text="lGetal1" Visible="False"></asp:Label>
+                    <asp:Label ID="lBereken" runat="server" Text="lBereken" Visible="False"></asp:Label>
+                </td>
+                <td class="auto-style1"></td>
+                <td class="auto-style1"></td>
+            </tr>
+            <tr>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+            </tr>
+        </table>
+    </form>
+</body>
+</html>
---
+++ Opdracht6/Default.aspx.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        this.UnobtrusiveValidationMode = UnobtrusiveValidationMode.None;
+    }
+
+
+    protected void btn1_Click(object sender, EventArgs e)
+    {
+        //controleer of er minder dan 7 cijfers staan
+        if(canWrite())
+        {
+            txtScherm.Text += 1;
+        }
+    }
+    protected void btn2_Click(object sender, EventArgs e)
+    {
+        //controleer of er minder dan 7 cijfers staan
+        if (canWrite())
+        {
+            txtScherm.Text += 2;
+        }
+    }
+    protected void btn3_Click(object sender, EventArgs e)
+    {
+        //controleer of er minder dan 7 cijfers staan
+        if (canWrite())
+        {
+            txtScherm.Text += 3;
+        }
+    }
+    protected void btn4_Click(object sender, EventArgs e)
+    {
+        //controleer of er minder dan 7 cijfers staan
+        if (canWrite())
+        {
+            txtScherm.Text += 4;
+        }
+    }
+    protected void btn5_Click(object sender, EventArgs e)
+    {
+        //controleer of er minder dan 7 cijfers staan
+        if (canWrite())
+        {
+            txtScherm.Text += 5;
+        }
+    }
+    protected void btn6_Click(object sender, EventArgs e)
+    {
+        //controleer of er minder dan 7 cijfers staan
+        if (canWrite())
+        {
+            txtScherm.Text += 6;
+        }
+    }
+    protected void btn7_Click(object sender, EventArgs e)
+    {
+        //controleer of er minder dan 7 cijfers staan
+        if (canWrite())
+        {
+            txtScherm.Text += 7;
+        }
+    }
+    protected void btn8_Click(object sender, EventArgs e)
+    {
+        //controleer of er minder dan 7 cijfers staan
+        if (canWrite())
+        {
+            txtScherm.Text += 8;
+        }
+    }
+    protected void btn9_Click(object sender, EventArgs e)
+    {
+        //controleer of er minder dan 7 cijfers staan
+        if (canWrite())
+        {
+            txtScherm.Text += 9;
+        }
+    }
+    protected void btn0_Click(object sender, EventArgs e)
+    {
+        //controleer of er minder dan 7 cijfers staan en 0 niet het 1e cijfer is.
+        if (canWrite() && txtScherm.Text.Length !=0)
+        {
+            txtScherm.Text += 0;
+        }
+    }
+    protected void btnBack_Click(object sender, EventArgs e)
+    {
+        if(txtScherm.Text.Length > 0)
+             txtScherm.Text= txtScherm.Text.Substring(0, txtScherm.Text.Length-1);
+
+    }
+    protected void btnC_Click(object sender, EventArgs e)
+    {
+        txtScherm.Text = ""; //reset
+    }
+
+    protected void btnPlus_Click(object sender, EventArgs e)
+    {
+        lGetal1.Text = txtScherm.Text;
+        txtScherm.Text = string.Empty;
+        lBereken.Text = "+";
+    }
+
+    protected void btnMin_Click(object sender, EventArgs e)
+    {
+        lGetal1.Text = txtScherm.Text;
+        txtScherm.Text = string.Empty;
+        lBereken.Text = "-";
+    }
+    protected void btnIs_Click(object sender, EventArgs e)
+    {
+        //maak vars
+        double G1 = Convert.ToDouble(txtScherm.Text);
+        double G2 = Convert.ToDouble(lGetal1.Text);
+        double uitkomst = 0;
+
+        //bereken
+        if (lBereken.Text == "+")
+        {
+            uitkomst = G1 + G2;
+        }
+        else if (lBereken.Text == "-")
+        {
+            uitkomst = G1 - G2;
+        }
+        //plaats berekening 
+        txtScherm.Text = uitkomst.ToString();
+
+    }
+
+    protected void btnPunt_Click(object sender, EventArgs e)
+    {
+        if (!txtScherm.Text.Contains(","))
+        {
+            if(txtScherm.Text.Length == 0)
+            {
+                txtScherm.Text += "0,";
+            }
+            else
+            {
+                txtScherm.Text += ",";
+            }
+        }
+    }
+
+
+    public bool canWrite()
+    {
+        if (txtScherm.Text.Length < 7)
+            return true;
+        else
+        {
+            lbl_max.Visible = true;
+            return false;
+        }
+    }
+}
